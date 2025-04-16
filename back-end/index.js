16a2184b6568df4398508b9418619634ec562c2e
marimuthu37 <marimuthug037@gmail.com>
2025-04-16 12:51:24 +0530

    .

--- back-end/index.js
+++ back-end/index.js
@@ -9,12 +9,13 @@ const path = require("path");
 const app = express();
 app.use(cors());
 app.use(express.json());
+require("dotenv").config();
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "#mari37",
-  database: "qb",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 db.connect((err) => {
