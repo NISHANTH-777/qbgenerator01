00e76fdb59ee625c87f04d39269d3dd00a70322c
marimuthu37 <marimuthug037@gmail.com>
2025-05-08 10:56:31 +0530

    .

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,11 +1,33 @@
 // src/store/store.js
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './userSlice';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
 
+// Configure persistence
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['user'], // only user will be persisted
+};
+
+// Create persisted reducer
+const persistedReducer = persistReducer(persistConfig, userReducer);
+
+// Configure store with persisted reducer
 const store = configureStore({
   reducer: {
-    user: userReducer,
+    user: persistedReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
+// Create persistor
+export const persistor = persistStore(store);
 export default store;
